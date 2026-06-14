d3edc5e839ff46d0f6cf44d583996065bd953337
Oluwafemi Akinpelu <akinpelufemi05@gmail.com>
2026-06-14 22:03:14 +0100

    Imported the ProductListing using the data.json

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,5 +1,14 @@
+import ProductListings from "./ProductListings";
+import Data from "../data.json";
 const ProductsSection = () => {
-  return <div>ProductsSection</div>;
+  return (
+    <div className="space-y-2">
+      <h1 className="text-4xl font-bold mx-1 my-2">Desserts</h1>
+      <div>
+        <ProductListings products={Data} />
+      </div>
+    </div>
+  );
 };
 
 export default ProductsSection;
